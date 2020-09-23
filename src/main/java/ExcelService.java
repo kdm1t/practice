bc8a7adbcdf277041669f76d9fd3dad171ff56b9
Dmtriy Kuchma <dk@kalite.ro>
2020-09-23 17:52:44 +0300

    Еще разбил на методы + добавил стили для ячеек

--- src/main/java/ExcelService.java
+++ src/main/java/ExcelService.java
@@ -13,17 +13,19 @@ public class ExcelService {
     private Sheet currentSheet;
     private static int count = 0;
     private static int i = 0;
+    private CellStyle fullBorderStyle;
 
     public ExcelService() throws IOException {
 //        workbook = WorkbookFactory.create(new FileInputStream((getClass().getClassLoader().getResource("tabel.xls").getFile())));
         workbook = WorkbookFactory.create(new FileInputStream("C:\\tabel.xls"));
         currentSheet = workbook.getSheetAt(0);
+        makeFullBorderStyle();
     }
 
     public void addPersons(Person person) throws IOException {
         CellReference cR = getCellReference();
 
-        Row firstRow =  currentSheet.createRow(cR.getRow());
+        Row firstRow = currentSheet.createRow(cR.getRow());
         Row secondRow = currentSheet.createRow(cR.getRow() + 1);
 
         setCellFromPerson(person, firstRow, secondRow);
@@ -37,13 +39,70 @@ public class ExcelService {
         saveBookAndClose();
     }
 
+    private CellReference getCellReference() {
+        CellReference cR = new CellReference("A15");
+        for (i = 15; currentSheet.getRow(cR.getRow()).getCell(cR.getCol()) != null; i += 2) {
+            cR = new CellReference("A" + i);
+            count++;
+        }
+        return cR;
+    }
+
+    private void setCellFromPerson(Person person, Row firstRow, Row secondRow) {
+        CellStyle fioStyle = workbook.createCellStyle();
+        fioStyle.setBorderBottom(BorderStyle.THIN);
+        fioStyle.setBorderTop(BorderStyle.THIN);
+        fioStyle.setAlignment(HorizontalAlignment.RIGHT);
+
+        firstRow.createCell(0).setCellStyle(fioStyle);
+        secondRow.createCell(0).setCellStyle(fioStyle);
+        firstRow.getCell(0).setCellValue(count);
+        currentSheet.addMergedRegion(new CellRangeAddress(firstRow.getRowNum(), secondRow.getRowNum(), 0, 0));
+
+        firstRow.createCell(1).setCellStyle(fioStyle);
+        secondRow.createCell(1).setCellStyle(fioStyle);
+        firstRow.getCell(1).setCellValue(".");
+        currentSheet.addMergedRegion(new CellRangeAddress(firstRow.getRowNum(), secondRow.getRowNum(), 1, 1));
+
+        fioStyle.setWrapText(true);
+        fioStyle.setAlignment(HorizontalAlignment.LEFT);
+        firstRow.createCell(2).setCellStyle(fioStyle);
+        secondRow.createCell(2).setCellStyle(fioStyle);
+        firstRow.getCell(2).setCellValue(person.getFio());
+        currentSheet.addMergedRegion(new CellRangeAddress(firstRow.getRowNum(), secondRow.getRowNum(), 2, 2));
+
+        for (int k = 3; k < 5; k++) {
+            firstRow.createCell(k).setCellStyle(fullBorderStyle);
+            secondRow.createCell(k).setCellStyle(fullBorderStyle);
+        }
+        firstRow.getCell(3).setCellValue(person.getNumber());
+        currentSheet.addMergedRegion(new CellRangeAddress(firstRow.getRowNum(), secondRow.getRowNum(), 3, 4));
+
+        for (int k = 5; k < 7; k++) {
+            firstRow.createCell(k).setCellStyle(fullBorderStyle);
+            secondRow.createCell(k).setCellStyle(fullBorderStyle);
+        }
+        firstRow.getCell(5).setCellValue(person.getProfession());
+        currentSheet.addMergedRegion(new CellRangeAddress(firstRow.getRowNum(), secondRow.getRowNum(), 5, 6));
+    }
+
+    private void setCellWithFirstPartOfDays(Person person, Row row) {
+        for (int k = 0; k < 3; k++) {
+            row.createCell(i + k).setCellStyle(fullBorderStyle);
+        }
+        currentSheet.addMergedRegion(new CellRangeAddress(row.getRowNum(), row.getRowNum(), i, i + 2));
+        row.getCell(i).setCellValue(person.getFirstPartSumDays());
+        i += 3;
+    }
+
     private void setDaysToCells(Person person, Row row) {
         i = 7;
         for (ChoiceBox day : person.getDays()) {
             if (i == 22) {
                 setCellWithFirstPartOfDays(person, row);
             }
-            row.createCell(i).setCellValue(day.getValue().toString());
+            row.createCell(i).setCellStyle(fullBorderStyle);
+            row.getCell(i).setCellValue(day.getValue().toString());
             i++;
         }
     }
@@ -54,55 +113,15 @@ public class ExcelService {
             if (i == 22) {
                 setCellWithFirstPartOfHours(person, row);
             }
-            row.createCell(i).setCellValue(hours.getText());
+            row.createCell(i).setCellStyle(fullBorderStyle);
+            row.getCell(i).setCellValue(hours.getText());
             i++;
         }
     }
 
-    private CellReference getCellReference() {
-        CellReference cR = new CellReference("A15");
-        for (i = 15; currentSheet.getRow(cR.getRow()).getCell(cR.getCol()) != null; i += 2) {
-            cR = new CellReference("A" + i);
-            count++;
-        }
-        return cR;
-    }
-
-    private void setCellFromPerson(Person person, Row row, Row row1) {
-
-        row.createCell(0).setCellValue(count);
-        row1.createCell(0);
-        currentSheet.addMergedRegion(new CellRangeAddress(row.getRowNum(), row1.getRowNum(), 0, 0));
-        row.createCell(1).setCellValue(".");
-        row1.createCell(1);
-        currentSheet.addMergedRegion(new CellRangeAddress(row.getRowNum(), row1.getRowNum(), 1, 1));
-        row.createCell(2).setCellValue(person.getFio());
-        row1.createCell(2);
-        currentSheet.addMergedRegion(new CellRangeAddress(row.getRowNum(), row1.getRowNum(),  2,  2));
-        row.createCell(3).setCellValue(person.getNumber());
-        row1.createCell(3);
-        row.createCell(4);
-        row1.createCell(4);
-        currentSheet.addMergedRegion(new CellRangeAddress(row.getRowNum(), row1.getRowNum(),  3, 4));
-        row.createCell(5).setCellValue(person.getProfession());
-        row1.createCell(5);
-        row.createCell(6);
-        row1.createCell(6);
-        currentSheet.addMergedRegion(new CellRangeAddress(row.getRowNum(), row1.getRowNum(), 5, 6));
-    }
-
-    private void setCellWithFirstPartOfDays(Person person, Row row) {
-        for (int k = 0; k < 2; k++) {
-            row.createCell(i + k);
-        }
-        currentSheet.addMergedRegion(new CellRangeAddress(row.getRowNum(), row.getRowNum(), i, i + 2));
-        row.getCell(i).setCellValue(person.getFirstPartSumDays());
-        i += 3;
-    }
-
     private void setCellWithFirstPartOfHours(Person person, Row row) {
-        for (int k = 0; k < 2; k++) {
-            row.createCell(i + k);
+        for (int k = 0; k < 3; k++) {
+            row.createCell(i + k).setCellStyle(fullBorderStyle);
         }
         currentSheet.addMergedRegion(new CellRangeAddress(row.getRowNum(), row.getRowNum(), i, i + 2));
         row.getCell(i).setCellValue(person.getFirstPartSumHours());
@@ -110,14 +129,25 @@ public class ExcelService {
     }
 
     private void setCellsWithFullDaysAndHours(Person person, Row firstRow, Row secondRow) {
-        for (int k = 0; k < 2; k++) {
-            firstRow.createCell(i + k);
-            secondRow.createCell(i + k);
+        for (int k = 0; k < 3; k++) {
+            firstRow.createCell(i + k).setCellStyle(fullBorderStyle);
+            secondRow.createCell(i + k).setCellStyle(fullBorderStyle);
         }
         currentSheet.addMergedRegion(new CellRangeAddress(firstRow.getRowNum(), firstRow.getRowNum(), i, i + 2));
         currentSheet.addMergedRegion(new CellRangeAddress(secondRow.getRowNum(), secondRow.getRowNum(), i, i + 2));
+
         firstRow.getCell(i).setCellValue(person.getSumDays());
         secondRow.getCell(i).setCellValue(person.getSumHours());
+    }
+
+    private void makeFullBorderStyle() {
+        fullBorderStyle = workbook.createCellStyle();
+        fullBorderStyle.setBorderLeft(BorderStyle.THIN);
+        fullBorderStyle.setBorderTop(BorderStyle.THIN);
+        fullBorderStyle.setBorderRight(BorderStyle.THIN);
+        fullBorderStyle.setBorderBottom(BorderStyle.THIN);
+        fullBorderStyle.setAlignment(HorizontalAlignment.CENTER);
+        fullBorderStyle.setVerticalAlignment(VerticalAlignment.CENTER);
     }
 
     private void saveBookAndClose() throws IOException {
@@ -129,4 +159,5 @@ public class ExcelService {
         }
         workbook.close();
     }
+
 }
